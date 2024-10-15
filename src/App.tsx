6a9faade54542be4be7577b9b53402bbbacf4747
artur <alveshalmeida@gmail.com>
2024-10-14 22:18:58 -0300

    FEAT/adicionando components e hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import './Style.css'
+import Header from "./components/Header";
+import Sidenav from "./components/Sidenav";
+import Resume from "./pages/Resume";
+import "./Style.css";
 
 function App() {
-
   return (
-    <p>Teste</p>
-  )
+  <div>
+    <Sidenav />
+    <main>
+    <Header />
+    <Resume />
+    </main>
+  </div>
+);
 }
 
-export default App
+export default App;
