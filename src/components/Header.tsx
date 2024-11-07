1b0ca5aa6fb71c8fe669af2125ba9641c83695a8
artur.alves <artur.henrique@defensoria.mg.def.br>
2024-11-06 22:24:44 -0300

    FEAT/adicionando icons e imagens e ajuste css sidenav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
+import React from "react";
 import DateRanger from "./DateRanger";
 import { Months } from "./Months";
 
 const Header = () => {
+  const [title, setTitle] = React.useState("Resumo");
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="daterange mb">
         <DateRanger />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Months />
     </header>
--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,5 +1,60 @@
+import resumeIcon from "../assets/icons/resumo.svg";
+import saleIcons from "../assets/icons/vendas.svg";
+import webhooksIcons from "../assets/icons/webhooks.svg";
+import configIcons from "../assets/icons/configuracoes.svg";
+import contactIcon from "../assets/icons/contato.svg";
+import exitIcon from "../assets/icons/sair.svg"
+import FintechSVG from "../assets/FintechSVG";
+
 const Sidenav = () => {
-  return <div>Sidenav</div>;
+  return (
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Logo Fintech" />
+      <ul>
+        <li>
+          <span>
+            <img src={resumeIcon} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+
+        <li>
+          <span>
+            <img src={saleIcons} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+
+        <li>
+          <span>
+            <img src={webhooksIcons} alt="" />
+          </span>
+          <a>Webhooks</a>
+        </li>
+
+        <li>
+          <span>
+            <img src={configIcons} alt="" />
+          </span>
+          <a>Configurações</a>
+        </li>
+
+        <li>
+          <span>
+            <img src={contactIcon} alt="" />
+          </span>
+          <a>Contato</a>
+        </li>
+
+        <li>
+          <span>
+            <img src={exitIcon} alt="" />
+          </span>
+          <a>Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidenav;
